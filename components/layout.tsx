2099f037657daf3bdbe2cd459386728188d239bc
sammy <exgin3538@yahoo.com>
2021-01-15 21:00:17 -0500

    finally fix icons on navbar, have to keep icons imported

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,6 +2,16 @@ import { useState } from 'react';
 
 import layoutStyles from '../styles/Home.module.css';
 
+import fontawesome from '@fortawesome/fontawesome';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faBars } from '@fortawesome/fontawesome-free-solid';
+
+fontawesome.library.add(faTwitter, faFacebook, faInstagram, faBars);
+
+console.log(fontawesome.library);
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
@@ -12,14 +22,14 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           <div className='container px-4 mx-auto flex flex-wrap items-center justify-between'>
             <div className='w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start'>
               <a className='text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white' href='#pablo'>
-                gray Tailwind Starter Kit
+                Sobre Las Alas Del Aguila
               </a>
               <button
                 className='text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none'
                 type='button'
                 onClick={() => setNavbarOpen(!navbarOpen)}
               >
-                <i className='fa fa-bars'>HEY</i>
+                <i className='fas fa-bars'></i>
               </button>
             </div>
             <div className={'lg:flex flex-grow items-center' + (navbarOpen ? ' flex' : ' hidden')} id='example-navbar-danger'>
@@ -29,17 +39,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                     className='px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75'
                     href='#pablo'
                   >
-                    <i className='fab fa-facebook-square text-lg leading-lg text-white opacity-75'></i>
-                    <span className='ml-2'>Share</span>
-                  </a>
-                </li>
-                <li className='nav-item'>
-                  <a
-                    className='px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75'
-                    href='#pablo'
-                  >
                     <i className='fab fa-twitter text-lg leading-lg text-white opacity-75'></i>
-                    <span className='ml-2'>Tweet</span>
+                    <span className='ml-2'>Twitter</span>
                   </a>
                 </li>
                 <li className='nav-item'>
@@ -47,8 +48,17 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                     className='px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75'
                     href='#pablo'
                   >
-                    <i className='fab fa-pinterest text-lg leading-lg text-white opacity-75'></i>
-                    <span className='ml-2'>Pin</span>
+                    <i className='fab fa-facebook text-lg leading-lg text-white opacity-75'></i>
+                    <span className='ml-2'>Facebook</span>
+                  </a>
+                </li>
+                <li className='nav-item'>
+                  <a
+                    className='px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75'
+                    href='#pablo'
+                  >
+                    <i className='fab fa-instagram text-lg leading-lg text-white opacity-75'></i>
+                    <span className='ml-2'>instagram</span>
                   </a>
                 </li>
               </ul>
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <Layout>
       <Head>
-        <title>Create Next App</title>
+        <title>Sobre</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
